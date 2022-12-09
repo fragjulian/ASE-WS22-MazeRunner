be4bee10cde53b8f896b82b8c56722bb6514f20f
Gregor <gregor2@outlook.at>
2022-12-09 16:22:23 +0100

    return image instead of text

--- BE_MazeRunner/src/main/java/com/example/mazerunner/MazeSolverController.java
+++ BE_MazeRunner/src/main/java/com/example/mazerunner/MazeSolverController.java
@@ -1,17 +1,23 @@
 package com.example.mazerunner;
 
 
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+
 @RestController
 public class MazeSolverController {
-    @PostMapping("/api/maze")
-    public String uploadImage(@RequestParam("image") MultipartFile file) {
+    @PostMapping(
+            value = "/api/maze",
+            produces = MediaType.IMAGE_JPEG_VALUE
+    )
+    public byte[] uploadImage(@RequestParam("image") MultipartFile file) throws IOException {
         //solve maze and return the solved image
-        return "image uploaded";
+        return file.getBytes();
     }
 
 }
