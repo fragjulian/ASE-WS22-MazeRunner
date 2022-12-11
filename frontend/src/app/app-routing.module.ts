d71a82afdf98e2308843ab883d4cb74d1e1332cc
Poetzi <p.stephan@gmx.at>
2022-12-11 12:27:18 +0100

    Changed a few comments. Tried a REST Api for image upload. But did not work so I deleted it.

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -3,8 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import {AboutComponent} from "./about/about.component";
 import {MrUploadFileComponent} from "./upload/mr-upload-file.component";
 
+
+
 const routes: Routes = [
+  //default path
+  {path: '', component: MrUploadFileComponent},
+  //about path
   {path: 'about', component: AboutComponent},
+  //home or upload path
   {path: 'home', component: MrUploadFileComponent}
 ];
 
