8ec31c67dae90c97c8224f7a4e4b4edeb7eaaa91
Gregor <gregor2@outlook.at>
2022-12-18 15:19:23 +0100

    fixed bug in distance calculation in EuclideanDistance.java

--- backend/src/main/java/com/example/mazerunner/EuclideanDistance.java
+++ backend/src/main/java/com/example/mazerunner/EuclideanDistance.java
@@ -3,6 +3,6 @@ package com.example.mazerunner;
 public class EuclideanDistance implements DistanceMetric {
     @Override
     public double getDistance(Position a, Position b) {
-        return Math.sqrt((a.x - b.x) ^ 2 + (b.x - b.y) ^ 2);
+        return Math.sqrt(Math.pow(b.x - a.x, 2) + Math.pow(b.y - a.y, 2));
     }
 }
