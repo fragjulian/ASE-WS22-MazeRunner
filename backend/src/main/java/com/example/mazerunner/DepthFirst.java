28e8f1b5fb1d87d4aab2b7009f78a901481c7707
Gregor <gregor2@outlook.at>
2022-12-19 18:41:29 +0100

    extracted local variable as suggested by @fragjulian

--- backend/src/main/java/com/example/mazerunner/DepthFirst.java
+++ backend/src/main/java/com/example/mazerunner/DepthFirst.java
@@ -13,11 +13,12 @@ public class DepthFirst implements SearchStrategy {
             Position cheapestNeighbour = null;
             for (int x = current.getX() - 1; x <= current.getX() + 1; x++) {
                 for (int y = current.getY() - 1; y <= current.getY() + 1; y++) {
+                    Position currentNeighbour = new Position(x, y);
                     //only iterate over neighbours or valid pixels
-                    if ((x == current.getX() && y == current.getY()) || x < 0 || x >= bufferedImage.getWidth() || y < 0 || y >= bufferedImage.getHeight() || walls[x][y] || alreadyVisited.contains(new Position(x, y)))
+                    if ((x == current.getX() && y == current.getY()) || x < 0 || x >= bufferedImage.getWidth() || y < 0 || y >= bufferedImage.getHeight() || walls[x][y] || alreadyVisited.contains(currentNeighbour))
                         continue;
-                    if (cheapestNeighbour == null || heuristic.getHeuristic(cheapestNeighbour) > heuristic.getHeuristic(new Position(x, y)) && !alreadyVisited.contains(cheapestNeighbour))
-                        cheapestNeighbour = new Position(x, y);
+                    if (cheapestNeighbour == null || heuristic.getHeuristic(cheapestNeighbour) > heuristic.getHeuristic(currentNeighbour) && !alreadyVisited.contains(cheapestNeighbour))
+                        cheapestNeighbour = currentNeighbour;
                 }
             }
             //todo backtrack if we did not find a next cheapest neighbour
