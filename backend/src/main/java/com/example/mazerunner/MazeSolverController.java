c092d2eb609ad2ac757fe62fc1a543b4f4175260
Gregor <gregor2@outlook.at>
2022-12-22 12:35:25 +0100

    extracted object creation to factory class

--- backend/src/main/java/com/example/mazerunner/MazeSolverController.java
+++ backend/src/main/java/com/example/mazerunner/MazeSolverController.java
@@ -17,68 +17,36 @@ import java.io.IOException;
 @RestController
 public class MazeSolverController {
     private static final int IMAGE_TYPE = BufferedImage.TYPE_INT_RGB;
-    private static final int DEFAULT_WALL_COLOUR = new Color(0, 0, 0).getRGB();
-    private static final int DEFAULT_OBSTACLE_COLOUR = new Color(219, 219, 219).getRGB();
-    private static final int DEFAULT_SAFETY_DISTANCE = 1;
     private static final int PATH_COLOUR = new Color(255, 0, 0).getRGB();
     private static final int DEFAULT_BACKGROUND_COLOR = Color.WHITE.getRGB();
-    private static final DistanceMetric DEFAULT_DISTANCE_METRIC = new EuclideanDistance();
-    private static final WallDetector DEFAULT_WALL_DETECTOR = new ColourWallDetector(DEFAULT_WALL_COLOUR, DEFAULT_OBSTACLE_COLOUR, DEFAULT_SAFETY_DISTANCE, DEFAULT_DISTANCE_METRIC);
-    private final SearchStrategy DEFAULT_SEARCH_STRATEGY = new DepthFirst();
-    private final Heuristic DEFAULT_HEURISTIC = new RealDistanceHeuristic(DEFAULT_DISTANCE_METRIC);
+    private final MazeUtilsFactory mazeUtilsFactory = MazeUtilsFactory.getMazeUtilsFactory();
 
     /**
      * @param file                    multipart file containing the image
-     * @param wallDetectorParameter
-     * @param wallColourParameter
-     * @param obstacleColourParameter
-     * @param safetyDistanceParameter
-     * @param heuristicParameter
-     * @param searchStrategyParameter
-     * @param distanceMetricParameter
-     * @return
+     * @param wallDetectorParameter   which wall detector to use. Possible options: colorwalldetector, default is colorwalldetector
+     * @param wallColourParameter     wallColor parameter for the wall detector can be set if wall detector uses such a parameter
+     * @param obstacleColourParameter obstacleColor parameter for the wall detector can be set if wall detector uses such a parameter
+     * @param safetyDistanceParameter minimum distance to obstacles measured by distanceMetric
+     * @param heuristicParameter      which heuristic to use. Possible options: realdistanceheuristic, default is realdistanceheuristic
+     * @param searchStrategyParameter which search strategy to use. Possible options: depthfirst, default is depthfirst
+     * @param distanceMetricParameter which distance metric to use. Possible options: euclidean, default is euclidean
+     * @return an image containing the solved maze
      * @throws IOException
      */
-    @PostMapping(
-            value = "/api/maze",
-            produces = MediaType.IMAGE_JPEG_VALUE
-    )
-    public byte[] uploadImage(@RequestParam("image") MultipartFile file,
-                              @RequestParam(name = "walldetector", required = false, defaultValue = "colourwalldetector") String wallDetectorParameter,
-                              @RequestParam(name = "wallcolour", required = false) Integer wallColourParameter,//unfortunately cannot use the constant here as default due to spring
+    @PostMapping(value = "/api/maze", produces = MediaType.IMAGE_JPEG_VALUE)
+    public byte[] uploadImage(@RequestParam("image") MultipartFile file, @RequestParam(name = "walldetector", required = false, defaultValue = "colourwalldetector") String wallDetectorParameter, @RequestParam(name = "wallcolour", required = false) Integer wallColourParameter,//unfortunately cannot use the constant here as default due to spring
                               @RequestParam(name = "obstaclecolour", required = false) Integer obstacleColourParameter,//unfortunately cannot use the constant here as default due to spring
                               @RequestParam(name = "safetydistance", required = false) Integer safetyDistanceParameter,//unfortunately cannot use the constant here as default due to spring
-                              @RequestParam(name = "heuristic", required = false, defaultValue = "realdistanceheuristic") String heuristicParameter,
-                              @RequestParam(name = "searchstrategy", required = false, defaultValue = "depthfirst") String searchStrategyParameter,
-                              @RequestParam(name = "distancemetric", required = false, defaultValue = "euclidean") String distanceMetricParameter) throws IOException {
+                              @RequestParam(name = "heuristic", required = false, defaultValue = "realdistanceheuristic") String heuristicParameter, @RequestParam(name = "searchstrategy", required = false, defaultValue = "depthfirst") String searchStrategyParameter, @RequestParam(name = "distancemetric", required = false, defaultValue = "euclidean") String distanceMetricParameter) throws IOException {
 
-        WallDetector wallDetector = switch (wallDetectorParameter) {
-            case "colourwalldetector" -> {
-                int wallColour = wallColourParameter == null ? DEFAULT_WALL_COLOUR : wallColourParameter;
-                int obstacleColour = obstacleColourParameter == null ? DEFAULT_OBSTACLE_COLOUR : obstacleColourParameter;
-                int safetyDistance = safetyDistanceParameter == null ? DEFAULT_SAFETY_DISTANCE : safetyDistanceParameter;
-                yield new ColourWallDetector(wallColour, obstacleColour, safetyDistance, DEFAULT_DISTANCE_METRIC);
-            }
-            default -> DEFAULT_WALL_DETECTOR;
-        };
-
-        DistanceMetric distanceMetric = switch (distanceMetricParameter) {
-            case "euclidean" -> new EuclideanDistance();
-            default -> DEFAULT_DISTANCE_METRIC;
-        };
-
-        Heuristic heuristic = switch (heuristicParameter) {
-            case "realdistanceheuristic" -> new RealDistanceHeuristic(distanceMetric);
-            default -> DEFAULT_HEURISTIC;
-        };
-        //solve maze and return the solved image
+        DistanceMetric distanceMetric = mazeUtilsFactory.getDistanceMetric(distanceMetricParameter);
+        WallDetector wallDetector = mazeUtilsFactory.getWallDetector(wallDetectorParameter, wallColourParameter, obstacleColourParameter, safetyDistanceParameter, distanceMetric);
+        Heuristic heuristic = mazeUtilsFactory.getHeuristic(heuristicParameter, distanceMetric);
+        SearchStrategy searchStrategy = mazeUtilsFactory.getSearchStrategy(searchStrategyParameter);
         File temp = File.createTempFile("maze", ".temp");
         file.transferTo(temp);
         BufferedImage bufferedImage = ImageIO.read(temp);
-        Maze maze = new Maze(bufferedImage, heuristic, wallDetector, switch (searchStrategyParameter) {
-            case "depthfirst" -> new DepthFirst();
-            default -> DEFAULT_SEARCH_STRATEGY;
-        }, IMAGE_TYPE, PATH_COLOUR, DEFAULT_BACKGROUND_COLOR);
+        Maze maze = new Maze(bufferedImage, heuristic, wallDetector, searchStrategy, IMAGE_TYPE, PATH_COLOUR, DEFAULT_BACKGROUND_COLOR);
         bufferedImage = maze.solveMaze();
 
         ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
---
+++ backend/src/main/java/com/example/mazerunner/MazeUtilsFactory.java
@@ -0,0 +1,50 @@
+package com.example.mazerunner;
+
+import java.awt.*;
+
+public class MazeUtilsFactory {
+    private static final int DEFAULT_WALL_COLOUR = new Color(0, 0, 0).getRGB();
+    private static final int DEFAULT_OBSTACLE_COLOUR = new Color(219, 219, 219).getRGB();
+    private static final int DEFAULT_SAFETY_DISTANCE = 1;
+    private static final MazeUtilsFactory mazeUtilsFactory = new MazeUtilsFactory();
+
+    private MazeUtilsFactory() {
+    }
+
+    public static MazeUtilsFactory getMazeUtilsFactory() {
+        return mazeUtilsFactory;
+    }
+
+    public WallDetector getWallDetector(String wallDetector, Integer wallColor, Integer obstacleColor, Integer safetyDistance, DistanceMetric distanceMetric) {
+        return switch (wallDetector) {
+            case "colorwalldetector" -> {
+                int wallColorInt = wallColor == null ? DEFAULT_WALL_COLOUR : wallColor;
+                int obstacleColorInt = obstacleColor == null ? DEFAULT_OBSTACLE_COLOUR : obstacleColor;
+                int safetyDistanceInt = safetyDistance == null ? DEFAULT_SAFETY_DISTANCE : safetyDistance;
+                yield new ColourWallDetector(wallColorInt, obstacleColorInt, safetyDistanceInt, new EuclideanDistance());
+            }
+            default -> new ColourWallDetector(wallColor, obstacleColor, safetyDistance, distanceMetric);
+        };
+    }
+
+    public DistanceMetric getDistanceMetric(String distanceMetricParameter) {
+        return switch (distanceMetricParameter) {
+            case "euclidean" -> new EuclideanDistance();
+            default -> new EuclideanDistance();
+        };
+    }
+
+    public Heuristic getHeuristic(String heuristic, DistanceMetric distanceMetric) {
+        return switch (heuristic) {
+            case "realdistanceheuristic" -> new RealDistanceHeuristic(distanceMetric);
+            default -> new RealDistanceHeuristic(distanceMetric);
+        };
+    }
+
+    public SearchStrategy getSearchStrategy(String searchStrategy) {
+        return switch (searchStrategy) {
+            case "depthfirst" -> new DepthFirst();
+            default -> new DepthFirst();
+        };
+    }
+}
