f5d6b2eaec754963493eb0909771ea36863bbfca
Poetzi <p.stephan@gmx.at>
2023-01-02 16:44:30 +0100

    Added comments to rest-upload

--- frontend/src/app/rest-upload/rest-upload.component.ts
+++ frontend/src/app/rest-upload/rest-upload.component.ts
@@ -1,6 +1,5 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {RestService} from "./rest.service";
-
 
 @Component({
   selector: 'app-rest-upload',
@@ -8,8 +7,7 @@ import {RestService} from "./rest.service";
   styleUrls: ['./rest-upload.component.css']
 })
 export class RestUploadComponent {
-  constructor(private rest: RestService) {}
-
+  // Declare instance variables
   uploadImage: any;
   solvedMaze: any;
   successResponse: any;
@@ -18,18 +16,28 @@ export class RestUploadComponent {
   errorMessage: any;
   showPopup = false;
 
+  // Inject the RestService
+  constructor(private rest: RestService) {
+  }
+
+  // Event handler for when a file is selected
   onSelect(event: any) {
     this.uploadImage = event.addedFiles[0];
   }
 
+  // Event handler for when a file is removed
   onRemove(event: any) {
     this.uploadImage = null;
   }
 
+  // Function to submit the image to the REST API for solving
   imageUploadAction() {
+    // Reset the solved maze image and error message
     this.solvedMaze = null;
     this.errorMessage = null;
     this.showPopup = false;
+
+    // Create a new FormData object to hold the image and color values
     const transformedImage = new FormData();
     transformedImage.append('image', this.uploadImage, this.uploadImage.name);
     if (this.rgbvaluewall) {
@@ -39,7 +47,7 @@ export class RestUploadComponent {
       transformedImage.append('obstaclecolor', this.rgbvalueobstacle);
     }
 
-    //Helper function to turn blob response into a DataUrl for the image tags in HTML (https://stackoverflow.com/questions/18650168/convert-blob-to-base64)
+    // Helper function to convert blob response to DataURL
     const blobToBase64 = (blob: Blob) => {
       const reader = new FileReader();
       reader.readAsDataURL(blob);
@@ -50,28 +58,33 @@ export class RestUploadComponent {
       });
     };
 
+    // Call the solveMaze function of the RestService to send the HTTP request
     this.rest.solveMaze(transformedImage).subscribe(
+      // If the request is successful, store the response and display the solved maze image
       (response: any) => {
         console.log(response);
         this.successResponse = 'Answer from server';
         blobToBase64(response).then(res => {
-          //Used this link to know how to use the DataURL: https://en.wikipedia.org/wiki/Data_URI_scheme
           this.solvedMaze = res;
         });
         this.showPopup = true;
       },
+      // If the request fails, store the error message
       (error: any) => {
         console.error(error);
-        // Set the error message to be displayed to the user
         this.errorMessage = error.message;
       }
     );
   }
 
+  // Function to download the solved maze image
   downloadImage() {
+    // Create an anchor element and set its href to the DataURL of the solved maze image
     const link = document.createElement('a');
     link.href = this.solvedMaze;
+    // Set the download file name
     link.download = 'solved-maze.jpg';
+    // Click the link to trigger the download
     link.click();
   }
 }
