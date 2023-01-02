e75927dedac6ce914c78dcd08a62c40f2d9eca46
Poetzi <p.stephan@gmx.at>
2023-01-02 16:32:25 +0100

    added a test for the rest service with mocking

--- frontend/src/app/rest-upload/rest.service.spec.ts
+++ frontend/src/app/rest-upload/rest.service.spec.ts
@@ -1,10 +1,12 @@
-import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+
 import { RestService } from './rest.service';
+import {HttpErrorResponse, HttpHeaders} from "@angular/common/http";
 
 describe('RestService', () => {
   let service: RestService;
-  let httpTestingController: HttpTestingController;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -12,14 +14,32 @@ describe('RestService', () => {
       providers: [RestService]
     });
     service = TestBed.inject(RestService);
-    httpTestingController = TestBed.inject(HttpTestingController);
+    httpMock = TestBed.inject(HttpTestingController);
   });
+
+  it('should send a POST request to the correct URL with the correct payload and return the expected response', () => {
+    // Define the test data
+    const formData = new FormData();
+    formData.append('file', new File([], 'test.png'));
+    const mockResponse = new Blob();
+
+    // Call the service method
+    service.solveMaze(formData).subscribe(response => {
+      expect(response).toEqual(mockResponse);
+    });
+
+    // Expect a single request to be made to the specified URL with the correct payload
+    const req = httpMock.expectOne('http://localhost:8081/api/maze/colorwalldetector/realdistanceheuristic/depthfirst');
+    expect(req.request.method).toBe('POST');
+    expect(req.request.body).toEqual(formData);
+
+    // Set the mock response
+    req.flush(mockResponse);
+  });
+
 
   afterEach(() => {
-    httpTestingController.verify();
-  });
-
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+    // Ensure that there are no outstanding requests
+    httpMock.verify();
   });
 });
