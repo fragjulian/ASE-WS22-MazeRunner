bfa87123396397be2ee0faf7dbc6e63c75a19f0b
Gregor <gregor2@outlook.at>
2023-01-09 20:52:43 +0100

    fixed failing frontend test

--- frontend/src/app/maze-builder-auto-solve.service.spec.ts
+++ frontend/src/app/maze-builder-auto-solve.service.spec.ts
@@ -1,12 +1,15 @@
 import {TestBed} from '@angular/core/testing';
 
 import {MazeBuilderAutoSolveService} from './maze-builder-auto-solve.service';
+import {HttpClientTestingModule} from "@angular/common/http/testing";
 
 describe('MazeBuilderAutoSolveService', () => {
   let service: MazeBuilderAutoSolveService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule]
+    });
     service = TestBed.inject(MazeBuilderAutoSolveService);
   });
 
