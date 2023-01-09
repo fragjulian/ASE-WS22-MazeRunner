b02923b35177880b86b15a83066a1acab51336ef
Gregor <gregor2@outlook.at>
2023-01-09 17:05:43 +0100

    extracted long condition to separate method

--- backend/src/main/java/com/example/mazerunner/SquareDistance.java
+++ backend/src/main/java/com/example/mazerunner/SquareDistance.java
@@ -16,9 +16,10 @@ public class SquareDistance implements DistanceMetric {
         List<Position> result = new LinkedList<>();
         for (int x = position.getX() - 1; x <= position.getX() + 1; x++) {
             for (int y = position.getY() - 1; y <= position.getY() + 1; y++) {
-                if ((x == position.getX() && y == position.getY()) || x < 0 || x >= width || y < 0 || y >= height || !(x == position.getX() || y == position.getY()))
+                Position neighbour = new Position(x, y);
+                if (!isNeighbouringPosition(position, neighbour, width, height))
                     continue;
-                result.add(new Position(x, y));
+                result.add(neighbour);
             }
         }
 
@@ -29,5 +30,22 @@ public class SquareDistance implements DistanceMetric {
         }
         result.addAll(recursiveResult.stream().filter(p -> !result.contains(p)).toList());
         return result;
+    }
+
+    /**
+     * check if neighbour is a valid neighbouring pixel of currentPosition
+     *
+     * @param currentPosition the position in the middle of the neighbours
+     * @param neighbour       the position which you want to see if it is a neighbour
+     * @param width           max width of the image
+     * @param height          max height of the mase
+     * @return true if it is valid, false otherwise
+     */
+    private boolean isNeighbouringPosition(Position currentPosition, Position neighbour, int width, int height) {
+        return (!((neighbour.getX() == currentPosition.getX() && neighbour.getY() == currentPosition.getY()) ||
+                neighbour.getX() < 0 || neighbour.getX() >= width || neighbour.getY() < 0 || neighbour.getY() >= height ||
+                !(neighbour.getX() == currentPosition.getX() || neighbour.getY() == currentPosition.getY())) &&
+                this.getDistance(currentPosition, neighbour) == 1);
+
     }
 }
