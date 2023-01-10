ecc9427d0b2fe4a999cdcecfe22cbf5ff57ec0dc
Gregor <gregor2@outlook.at>
2023-01-10 18:19:01 +0100

    color dot in maze builder now updating correctly

--- frontend/src/app/maze-builder/maze-builder.component.spec.ts
+++ frontend/src/app/maze-builder/maze-builder.component.spec.ts
@@ -2,6 +2,8 @@ import {ComponentFixture, TestBed} from '@angular/core/testing';
 
 import {MazeBuilderComponent} from './maze-builder.component';
 import {FormsModule} from "@angular/forms";
+import {MazeBuilderAutoSolveService} from "../maze-builder-auto-solve.service";
+import {HttpClientTestingModule} from "@angular/common/http/testing";
 
 describe('MazeBuilderComponent', () => {
   let component: MazeBuilderComponent;
@@ -9,8 +11,8 @@ describe('MazeBuilderComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ MazeBuilderComponent ],
-      imports: [FormsModule]
+      declarations: [MazeBuilderComponent],
+      imports: [HttpClientTestingModule, FormsModule, MazeBuilderAutoSolveService]
     })
     .compileComponents();
 
